3c77f679d742167615584c87b9b1a58303459b75
Nathan Chere <git@nathanchere.com.au>
2014-03-10 13:43:52 +1100

    Change where credentials are pulled

--- CovertTweeter.Core/TweetRepository.cs
+++ CovertTweeter.Core/TweetRepository.cs
@@ -2,12 +2,38 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using Microsoft.Win32;
 using TweetSharp;
 
 namespace CovertTweeter.Core
 {
     public class TweetRepository
     {
+        private readonly string _apiKey;
+        private readonly string _apiKeySecret;
+        private readonly string _accessToken;
+        private readonly string _accessTokenSecret;
+
+        private readonly string _regPath = @"HKEY_CURRENT_USER\SOFTWARE\NathanChere\CovertTweeter";
+
+        public TweetRepository(string apiKey, string apiKeySecret, string accessToken, string accessTokenSecret)
+        {
+            _apiKey = apiKey;
+            _apiKeySecret = apiKeySecret;
+            _accessToken = accessToken;
+            _accessTokenSecret = accessTokenSecret;
+        }
+
+        public TweetRepository()
+        {
+            
+            (string)Registry.GetValue(_regPath, "Installed", null);    
+            _apiKey = apiKey;
+            _apiKeySecret = apiKeySecret;
+            _accessToken = accessToken;
+            _accessTokenSecret = accessTokenSecret;
+        }
+
         public List<TwitterStatus> GetTweets()
         {
             var ConsumerKey = ConfigurationManager.AppSettings["consumerKey"];
