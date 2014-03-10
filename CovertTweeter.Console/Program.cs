b9c48cbf34e270f1715ef294bdbf4e5733d42022
Nathan Chere <git@nathanchere.com.au>
2014-03-10 13:08:05 +1100

    Fix up namespacing issues

--- CovertTweeter.Console/Program.cs
+++ CovertTweeter.Console/Program.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
-namespace CovertTweeter.Console
+namespace CovertTweeter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var exitEvent = new ManualResetEvent(false);
 
+            Console.CancelKeyPress += (sender, eventArgs) => {
+                                          eventArgs.Cancel = true;
+                                          exitEvent.Set();
+                                      };
+
+            var server = new MyServer();     // example
+            server.Run();
+
+            exitEvent.WaitOne();
+            server.Stop();
         }
     }
 }
