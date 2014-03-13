5acd3cc83abd585eb930e21fa37639f350d75822
Nathan Chere <git@nathanchere.com.au>
2014-03-13 16:30:25 +1100

    File structure shuffle

--- CovertTweeter.Core/TwitterError.cs
+++ CovertTweeter.Core/TwitterError.cs
@@ -5,23 +5,4 @@
         public string Message { get; set; }
         public int Code { get; set; }
     }
-
-    public class TwitterStatus
-    {
-        public string  CreatedAt { get; set; }
-        public long Id { get; set; }
-        public string Text { get; set; }
-
-        public string InReplyToScreenName { get; set; }
-        public long? InReplyToUserId { get; set; }
-        public TwitterStatus RetweededStatus { get; set; }
-
-        public TwitterUser User { get; set; }
-    }
-
-    public class TwitterUser
-    {
-        public string Name { get; set; }
-        public string ScreenName { get; set; }
-    }   
 }
