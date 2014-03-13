810f95cd0489b18877f2acc9b6ae684c178e7ae3
Nathan Chere <git@nathanchere.com.au>
2014-03-14 09:50:04 +1100

    NAmespace fix

---
+++ CovertTweeter.Core/ITweetRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace CovertTweeter.Core
+{
+    public interface ITweetRepository
+    {
+        event TweetRepository.NewTweetEvent NewTweet;
+        event TweetRepository.NewMessageEvent NewMessage;
+        event TweetRepository.NewFollowerEvent NewFollower;
+        event TweetRepository.NewFavouriteEvent NewFavourite;
+        event TweetRepository.HeartbeatEvent Heartbeat;
+        void Start();
+        void Pause();
+        void Stop();
+    }
+}
