8c2cb477339072023e7555cdbfdce5a1acac6f51
Nathan Chere <git@nathanchere.com.au>
2014-03-13 18:22:15 +1100

    Add event scaffolding for new followers and messages

--- CovertTweeter.Core/TweetRepository.cs
+++ CovertTweeter.Core/TweetRepository.cs
@@ -5,24 +5,27 @@ using System.Linq;
 using Microsoft.Win32;
 using Newtonsoft.Json;
 using Tweetinvi;
+using TweetinviCore.Events.EventArguments;
 using TweetinviCore.Interfaces.Streaminvi;
 
 namespace CovertTweeter.Core
 {
     public class TweetRepository
-    {
-        private const string REG_PATH = @"HKEY_CURRENT_USER\SOFTWARE\NathanChere\CovertTweeter";
-        private const string KEY_API = @"ApiKey";
-        private const string KEY_APIPRIVATE = @"ApiSecret";
-        private const string KEY_TOKEN = @"AccessToken";
-        private const string KEY_TOKENPRIVATE = @"AccessTokenSecret";
+    {        
+        const string REG_PATH = @"HKEY_CURRENT_USER\SOFTWARE\NathanChere\CovertTweeter";
 
         private readonly IUserStream _userStream;
-
+        
+        #region .ctor                
         public TweetRepository() :this(null,null,null,null) { }
 
         public TweetRepository(string apiKey, string apiKeySecret, string accessToken, string accessTokenSecret)
-        {                        
+        {      
+            const string KEY_API = @"ApiKey";
+            const string KEY_APIPRIVATE = @"ApiSecret";
+            const string KEY_TOKEN = @"AccessToken";
+            const string KEY_TOKENPRIVATE = @"AccessTokenSecret";
+  
             TwitterCredentials.SetCredentials(
                 GetConfigValue(accessToken,KEY_TOKEN),
                 GetConfigValue(accessTokenSecret, KEY_TOKENPRIVATE),
@@ -30,30 +33,54 @@ namespace CovertTweeter.Core
                 GetConfigValue(apiKeySecret, KEY_APIPRIVATE));
 
             _userStream = Stream.CreateUserStream();
-        }
 
-        public void Start()
-        {
-            _userStream.TweetCreatedByMe += (s, a) => { Console.WriteLine("I posted {0}", a.Tweet.Text); };
-            _userStream.TweetCreatedByFriend += (s, a) => { Console.WriteLine("{0} posted {1}", a.Tweet.Creator.Name, a.Tweet.Text); };
-            _userStream.MessageReceived += (s, a) => { Console.WriteLine("You received the message : {0}", a.Message.Text); };
-            _userStream.StartStream();
-        }
-
-        /// <summary>
-        /// Return the first non-null result out of the default, registry
-        ///  and app.config values for the given index
-        /// </summary>        
+            CreateEventBindings();
+        }        
         private string GetConfigValue(string defaultValue, string index)
         {
             var result = defaultValue 
-                ?? (string)Registry.GetValue(REG_PATH, KEY_API, null)
-                ?? ConfigurationManager.AppSettings[KEY_API];                        
+                ?? (string)Registry.GetValue(REG_PATH, index, null)
+                ?? ConfigurationManager.AppSettings[index];                        
             if(result == null) throw new ConfigurationErrorsException("No defaultValue for " + index + " found in registry or app.config");
             return result;
         }
+        #endregion
 
-        EnvironmentVariableTarget 
+        #region Events
+        public delegate void NewTweetEvent(TweetReceivedEventArgs e);
+        public delegate void NewMessageEvent(MessageEventArgs e);
+        public delegate void NewFollowerEvent(UserFollowedEventArgs e);
+
+        public event NewTweetEvent NewTweet;
+        public event NewMessageEvent NewMessage;
+        public event NewFollowerEvent NewFollower;
+
+        private void CreateEventBindings()
+        {            
+            _userStream.TweetCreatedByMe += (sender, args) => { if (NewTweet != null) NewTweet(args); };
+            _userStream.TweetCreatedByFriend += (sender, args) => { if (NewTweet != null) NewTweet(args); };
+
+            _userStream.MessageReceived += (sender, args) => { if (NewMessage!= null) NewMessage(args); };
+
+            _userStream.FollowedByUser += (sender, args) => { if (NewFollower != null) NewFollower(args); };
+        }
+        #endregion
+
+        public void Start()
+        {            
+            _userStream.StartStream();
+        }
+        
+        public void Pause()
+        {            
+            _userStream.PauseStream();
+        }
+
+        public void Stop()
+        {            
+            _userStream.StopStream();
+        }
+
         //var user = User.GetLoggedUser();
         
     }
